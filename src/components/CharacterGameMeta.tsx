a366ac9afaa60bbfc0f8ac97f1872788369bc5fb
dblatcher <dv.bltchr@gmail.com>
2024-04-16 04:56:43 +0100

    options in dialog

--- src/components/CharacterGameMeta.tsx
+++ src/components/CharacterGameMeta.tsx
@@ -4,25 +4,45 @@ import React, { useState } from "react";
 import { CharacterGame } from "./CharacterGame";
 import { HIRAGANA } from "@/lib/language/hiragana";
 import { ConstanentPicker } from "./ConstanentPicker";
-import { Box, TextField } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, TextField, Typography } from "@mui/material";
 
 
 export const CharacterGameMeta: React.FunctionComponent = () => {
 
+    const [optionsOpen, setOptionsOpen] = useState(false)
     const [constanents, setConstanents] = useState<string[]>(HIRAGANA.constanents)
     const [roundsPerGame, setRoundPerGame] = useState<number>(3)
 
     return <SpeechProvider>
         <Box>
-            <ConstanentPicker {...{ constanents, setConstanents }} options={HIRAGANA.constanents} />
-            <TextField label="rounds per game" value={roundsPerGame} type="number" onChange={(event) => {
-                const numberValue = Number(event.target.value)
-                if (isNaN(numberValue)) {
-                    return
-                }
-                setRoundPerGame(Math.floor(numberValue))
-            }} />
+            <Button onClick={() => { setOptionsOpen(true) }}>options</Button>
             <CharacterGame constanents={constanents} roundsPerGame={roundsPerGame} />
         </Box>
+        <Dialog open={optionsOpen} onClose={() => { setOptionsOpen(false) }}>
+            <DialogTitle>game options</DialogTitle>
+            <DialogContent>
+                <Grid container gap={1}>
+                    <Grid item xs={6} md={8} >
+                        <Typography variant="overline">Hiragana Characters to test</Typography>
+                        <ConstanentPicker {...{ constanents, setConstanents }} options={HIRAGANA.constanents} />
+                    </Grid>
+
+                    <Grid item xs={3} paddingTop={1}>
+                        <TextField label="rounds per game" 
+                        value={roundsPerGame} 
+                        type="number" onChange={(event) => {
+                            const numberValue = Number(event.target.value)
+                            if (isNaN(numberValue)) {
+                                return
+                            }
+                            setRoundPerGame(Math.floor(numberValue))
+                        }} />
+                    </Grid>
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => { setOptionsOpen(false) }}>done</Button>
+            </DialogActions>
+        </Dialog>
     </SpeechProvider>
 }
